bedd3bb4af77ba6e760c6fb6cf0ee74210a965ea
Bars <52255812+superbars@users.noreply.github.com>
2022-01-13 12:24:36 +0300

    user_version

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import netmiko
 from netmiko import ConnectHandler
 
-print("[!]Hello, first of all check your device type. Be sure this is the mikrotik and it has a mikrotik-routerOS[!]")
-ip_of_device = input('[*]Please, input ip of your router[*]\n')
-username = input('[*]Please, put down username[*]\n')
-password = input('[*]Please, put down password[*]\n')
-
-
-def connect_to_mikrotik(ip_address, username, password):
+def connect_to_mikrotik(list):
+    print("[!]Check your device type. Be sure this is the mikrotik and it has a mikrotik-routerOS[!]")
+    ip_of_device = input('[*]Please, input ip of your router[*]\n')
+    username = input('[*]Please, put down username[*]\n')
+    password = input('[*]Please, put down password[*]\n')
     print("[!]Please wait...[!]")
+
     mikrotik_main_router = {
         'device_type':'mikrotik_routeros',
-        'host':ip_address,
+        'host':ip_of_device,
         'port':'22',
         'username':username,
-        'password':password
+        'password':password,
+        'global_cmd_verify': False
+
     }
     sshCli = ConnectHandler(**mikrotik_main_router)
-    print(sshCli.find_prompt())
+    print("Beginning ban of ip list")
+    for ip in list:
+        command = "/ip firewall filter add chain=forward src-address=" + ip + " action=drop comment=BlockedByScript"
+        sshCli.send_command(command)
+        print("IP: " + ip + " was banned")
+
     sshCli.disconnect()
 
 ip_addresses = []
-def read_ip(filename):
+def read_ip():
     global ip_addresses
+    filename = input("[!]Please specify your ip list[!]\n")
     with open(filename,'r') as f:
         ip_addresses = f.read().split()
 
-connect_to_mikrotik(ip_of_device, username, password)
+read_ip()
+connect_to_mikrotik(ip_addresses)
